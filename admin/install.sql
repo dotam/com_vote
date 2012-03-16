7bf9120bfd97d4fd791b436447a35d1375c1e3c2
xuanthachkgc <nguyenxuanthach_sn1990@yahoo.com>
2012-03-16 21:22:43 +0700

    Update admin/install.sql

--- admin/install.sql
+++ admin/install.sql
@@ -5,14 +5,19 @@ CREATE TABLE `#__vote` (
   `title` varchar(255) not null,
   primary key(`id`)
 ) ENGINE=MyISAM auto_increment=0 default CHARSET=utf8;
+
 insert into `#__vote` (`id`,`title`) values (1,'Who is the best player in the world?');
 
+
 DROP TABLE IF EXISTS `#__vote_item`;
+
 CREATE TABLE `#__vote_item`(
 	`id` int unsigned not null auto_increment,
 	`voteid` int not null ,
 	`text` text not null,
+	`hits` int,
 	primary key(`id`)
 	
 	)ENGINE=MyISAM auto_increment=0 default CHARSET=utf8;
+
 insert into `#__vote_item` (`voteid`,`text`) values (1,'Messi'), (1,'Ronaldo'), (1,'Kaka'), (1,'Rooney');
