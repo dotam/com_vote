6a1ce5f57ce1be7d6032a8111c9d59d0302ea6e0
xuanthachkgc <nguyenxuanthach_sn1990@yahoo.com>
2012-03-20 11:23:34 +0700

    Update site/models/vote.php

--- site/models/vote.php
+++ site/models/vote.php
@@ -1,11 +1,29 @@
-<?php 
+<?php
 
-defined('_JEXEC') or die('Restricted access'); 
+defined('_JEXEC') or die('Restricted access');
 
+
+jimport( 'joomla.application.component.model' );
 class VoteModelVote extends JModel
 {
     
-    
+	/**
+	 * biến $id trong phương thức select_item này là "id" trong table "__vote"
+	 * biến này có đươc là do use chọn "title" trong table "__vote"
+	 * giá trị biến $id có đuoc từ lop controller
+	 */
+	function getSelect_item($id)
+	{
+		$id = (int) $id;
+		$db =& JFactory::getDBO();
+
+		$query = 'SELECT text, hits FROM #__vote_item WHERE voteid='.(int)$id;
+		$db->setQuery( $query );
+		$result = $db->loadRowList();
+
+		return $result; //ket quả này đưa lên site/views/vote/view.html.php
+	}
+
 }
 
 ?>
