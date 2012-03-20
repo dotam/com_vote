f5f6436b6ff27e832216037e05234fae33360c21
xuanthachkgc <nguyenxuanthach_sn1990@yahoo.com>
2012-03-20 11:24:11 +0700

    Update site/views/vote/view.html.php

--- site/views/vote/view.html.php
+++ site/views/vote/view.html.php
@@ -1,23 +1,21 @@
-<?php 
+<?php
 
 
 defined('_JEXEC') or die('Restricted access');
 
 
 jimport( 'joomla.application.component.view');
- 
-/**
- * HTML View class for the HelloWorld Component
- *
- * @package    HelloWorld
- */
- 
+
+
 class VoteViewVote extends JView
 {
-    function display($tpl = null)
-    {
-       
-    }
+ function display($tpl = null)
+	{
+		$result = $this->get( 'Select_item' ); //ko hieu, mình nghĩ là funstion từ lớp view "getSelect_item"
+		$this->assignRef( 'result',	$result );
+
+		parent::display($tpl);
+	}
 }
 
- ?>
+?>
