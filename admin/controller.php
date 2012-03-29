5fe18070207a45ff618ab8675c6b84d360ce5d46
Xuan Thach <david@ubuntu.(none)>
2012-03-29 10:09:32 +0700

    OK mot ti

---
+++ admin/controller.php
@@ -0,0 +1,17 @@
+<?php
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+
+class VotesController extends JController
+{
+
+	function display()
+	{
+		parent::display();
+	}
+}
+
+?>
